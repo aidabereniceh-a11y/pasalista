9a1267c1d05389e7b3957a6762c7a08b7283bce9
Aida Berenice <aidabereniceh@gmail.com>
2026-06-13 21:10:16 -0600

    Recrear lib/supabase.ts

---
+++ lib/supabase.ts
@@ -0,0 +1,6 @@
+﻿import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://uuqedqpuajoolrmmidps.supabase.co";
+const supabaseAnonKey = "sb_publishable_sS-yKk-pQxD-2vMYNhgRsQ_1nqBGXst";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
