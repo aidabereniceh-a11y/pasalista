a3b98355ae1f2254c4b7c1120fbdbbb6ce1442b7
Aida Berenice <aidabereniceh@gmail.com>
2026-06-16 19:09:00 -0600

    feat: agregar robots.txt para bloquear rutas privadas

---
+++ app/robots.ts
@@ -0,0 +1,20 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/dashboard",
+        "/checkin/",
+        "/asistencia/",
+        "/qr/",
+        "/pago/",
+        "/admin",
+        "/api/",
+      ],
+    },
+    sitemap: "https://pasalista.mx/sitemap.xml",
+  };
+}
